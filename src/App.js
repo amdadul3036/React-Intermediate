757f3258aa7e22560b5b233518d86ff69b87051f
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-30 04:53:02 -0700

    React Bootstrap single Card and Button Use

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button'
+import {Card} from 'react-bootstrap'
+
 import './App.css';
+import News from './Component/News/News';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="primary">REACT BOOTSTRAP </Button>{' '}
+      <Card>
+        <Card.Body>How much should I learn?</Card.Body>
+      </Card>
+     
+      <News></News>
+
     </div>
   );
 }
