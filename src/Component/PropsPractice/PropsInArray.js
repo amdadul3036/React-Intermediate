5790cd91579634957d959a696288328dafc58b89
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-04 10:27:36 -0700

    Onclick

--- src/Component/PropsPractice/PropsInArray.js
+++ src/Component/PropsPractice/PropsInArray.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const PropsInArray = (props) => {
+
+    function Click(){
+        alert("Button is clicked");
+    }
     return (
         <div>
-            <h2>I love fruits: {props.fruits.join(", ")} </h2>
+            <h2>I love fruits: {props.fruits.join(" , ")} </h2>
+            <button onClick = {Click}>Click Me</button>
         </div>
     );
 };
